7305161b4bbee30460c988ebcff2f6faab53b6d6
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-02 16:26:38 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,8 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from cryptography.hazmat.backends import default_backend
 from Crypto.Util.Padding import pad
 from Crypto.PublicKey import RSA
-from cryptography.hazmat.primitives import serialization
+from cryptography import x509
+from cryptography.hazmat.primitives.serialization import load_pem_x509_certificate
 import oci
 
 def load_private_key_from_string(pub_key_str):
@@ -18,15 +19,19 @@ def generate_random(length):
     random_key = ''.join(secrets.choice(characters) for _ in range(length))
     return random_key
 
-def read_key_from_vault(key_ocid):
+def load_certificate_from_string(cert_str):
+    cert_bytes = cert_str.encode('utf-8')
+    cert = load_pem_x509_certificate(cert_bytes, default_backend())
+    return cert.public_key()
+
+def read_certificate_from_vault(cert_ocid):
     signer = oci.auth.signers.get_resource_principals_signer()
     try:
         client = oci.secrets.SecretsClient({}, signer=signer)
-        key_content = client.get_secret_bundle(key_ocid).data.secret_bundle_content.content.encode('utf-8')
-        key_bytes = base64.b64decode(key_content)
-        return key_bytes
+        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content.encode('utf-8')
+        return cert_content
     except Exception as ex:
-        logging.error("ERROR: failed to retrieve the key from the vault - {}".format(ex))
+        logging.error("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
         raise
 
 def encrypt_symm(key, init_vector, value):
@@ -44,15 +49,16 @@ def encrypt_asymmetric(public_key, plaintext):
 def encryption_logic(payload, key_ocid):
     randomno = generate_random(16)
     init_vector = generate_random(16)
-    public_key_bytes = read_key_from_vault(key_ocid)
-    public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
+    #public_key_bytes = read_key_from_vault(key_ocid)
+    '''public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
     public_key_str = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
-    ).decode('utf-8')
-
-    logging.info("Public key: {}".format(public_key_str))
-    public_key = load_private_key_from_string(public_key_str) 
+    ).decode('utf-8')'''
+    cert_content = read_certificate_from_vault(key_ocid)
+    logging.info("Public key: {}".format(cert_content))
+    public_key = load_certificate_from_string(cert_content)
+    #public_key = load_private_key_from_string(public_key_str) 
     encrypted_data = encrypt_symm(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
     encrypted_key = encrypt_asymmetric(public_key, randomno)
     
