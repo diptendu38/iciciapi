ab42c782cea323411a326713124e04b0fe3fbed8
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-02 17:20:57 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,67 +1,58 @@
-import secrets
-import logging
-import string
 import base64
 from Crypto.Cipher import AES, PKCS1_OAEP
-from cryptography.hazmat.backends import default_backend
-from Crypto.Util.Padding import pad
 from Crypto.PublicKey import RSA
-from cryptography import x509
-from cryptography.hazmat.backends.openssl import backend as openssl_backend
-from cryptography.hazmat.primitives.serialization import load_pem_x509_certificate
+from Crypto.Util.Padding import unpad
 import oci
-
-def load_private_key_from_string(pub_key_str):
-    public_key = RSA.import_key(pub_key_str)
-    return public_key
-
-def generate_random(length):
-    characters = string.ascii_letters + string.digits
-    random_key = ''.join(secrets.choice(characters) for _ in range(length))
-    return random_key
+import logging
+from cryptography.hazmat.backends import default_backend
+from cryptography import x509
 
 def load_certificate_from_string(cert_str):
     cert_bytes = cert_str.encode('utf-8')
-    cert = load_pem_x509_certificate(cert_bytes, openssl_backend)
+    cert = x509.load_pem_x509_certificate(cert_bytes, default_backend())
+    return cert
+
+def extract_public_key_from_certificate(cert):
     return cert.public_key()
 
 def read_certificate_from_vault(cert_ocid):
     signer = oci.auth.signers.get_resource_principals_signer()
     try:
         client = oci.secrets.SecretsClient({}, signer=signer)
-        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content.encode('utf-8')
+        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content.decode('utf-8')
         return cert_content
     except Exception as ex:
         logging.error("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
         raise
 
-def encrypt_symm(key, init_vector, value):
-    cipher = AES.new(key, AES.MODE_CBC, init_vector)
-    padded_data = pad(value.encode('utf-8'), AES.block_size)
-    encrypted = cipher.encrypt(padded_data)
-    iv_and_ciphertext = init_vector + encrypted
-    return base64.urlsafe_b64encode(iv_and_ciphertext).decode('utf-8')
+def decrypt_symmetric(key, iv, ciphertext):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return decrypted.decode('utf-8')
 
-def encrypt_asymmetric(public_key, plaintext):
-    cipher = PKCS1_OAEP.new(public_key)
-    encrypted_data = cipher.encrypt(plaintext.encode('utf-8'))
-    return base64.b64encode(encrypted_data).decode('utf-8')
+def decrypt_asymmetric(b64_encrypted_msg, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted_msg = cipher.decrypt(base64.b64decode(b64_encrypted_msg))
+    return decrypted_msg
 
-def encryption_logic(payload, key_ocid):
-    randomno = generate_random(16)
-    init_vector = generate_random(16)
-    #public_key_bytes = read_key_from_vault(key_ocid)
-    '''public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
-    public_key_str = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    ).decode('utf-8')'''
-    cert_content = read_certificate_from_vault(key_ocid)
-    logging.info("Public key: {}".format(cert_content))
-    public_key = load_certificate_from_string(cert_content)
-    #public_key = load_private_key_from_string(public_key_str) 
-    encrypted_data = encrypt_symm(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
-    encrypted_key = encrypt_asymmetric(public_key, randomno)
-    
-    return encrypted_data, encrypted_key
+def decryption_logic(encrypted_data, encrypted_key, cert_ocid):
+    cert_content = read_certificate_from_vault(cert_ocid)
+    cert = load_certificate_from_string(cert_content)
+    public_key = extract_public_key_from_certificate(cert)
 
+    plain_key = decrypt_asymmetric(encrypted_key, public_key)
+    print(f"Decrypted Session Key: {plain_key.decode('utf-8')}")
+
+    iv_and_ciphertext = base64.urlsafe_b64decode(encrypted_data.encode('utf-8'))
+    iv = iv_and_ciphertext[:16]
+    ciphertext = iv_and_ciphertext[16:]
+
+    decrypted_payload = decrypt_symmetric(plain_key, iv, ciphertext)
+    print(f"Decrypted Payload: {decrypted_payload}")
+'''
+# Example usage
+encrypted_data = "b0NUU01yZ0I1c1dhUTlhenAPu-HcssSM8m6Ph1_Ok0vEnisudpHUTT4m49WvZQYxrvw1xjlUOrThsdTHyFcIxg=="
+encrypted_key = "n07Ypu0+UFIFzwnL/PxA9Eu+oJ5j20AAuzQ5xLWMEGqDZwHEAFUejC7pQczLAYnp5M9DLAQ3s/fM34Kt/jwX0rFHLnEStNqtMQl7/NTv7mOzafKVb0/KYjGuJu6096OXhLHvjk/BDB1DzqqfCgaPndGCPh202aO2BKKon5WbZiQVrsaOIhJ9/SiRqETA48On/lk/NLpyvdZ/dqUsfwouB9Ni2JY9ouqPjIrynCip813rpIpRZnfnFiBP4/n7AICyIutBZKcds6dsXbxBZW4mqBWKldq2/omqxCbW6P8IqHwZ3atKLy9W8gfKhe1swFPClLJH8T9wjh/ThxLPNqJwaQ=="
+cert_ocid = "your_certificate_ocid"
+
+decryption_logic(encrypted_data, encrypted_key, cert_ocid)'''
