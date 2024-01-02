3b8451c0b19bfde7fc957e527462a0daadcb2ad6
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-02 18:37:26 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,21 +1,35 @@
+import secrets
+import logging
 import string
 import base64
-import json
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from Crypto.Util.Padding import pad
-import secrets
-import oci
 from cryptography.hazmat.backends import default_backend
+from Crypto.Util.Padding import pad
+from Crypto.PublicKey import RSA
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.serialization import load_pem_x509_certificate
+import oci
+
+def load_private_key_from_string(pub_key_str):
+    public_key = RSA.import_key(pub_key_str)
+    return public_key
 
 def generate_random(length):
     characters = string.ascii_letters + string.digits
     random_key = ''.join(secrets.choice(characters) for _ in range(length))
     return random_key
 
-def encrypt_symmetric(key, init_vector, value):
+def read_key_from_vault(key_ocid):
+    signer = oci.auth.signers.get_resource_principals_signer()
+    try:
+        client = oci.secrets.SecretsClient({}, signer=signer)
+        key_content = client.get_secret_bundle(key_ocid).data.secret_bundle_content.content.encode('utf-8')
+        key_bytes = base64.b64decode(key_content)
+        return key_bytes
+    except Exception as ex:
+        logging.error("ERROR: failed to retrieve the key from the vault - {}".format(ex))
+        raise
+
+def encrypt_symm(key, init_vector, value):
     cipher = AES.new(key, AES.MODE_CBC, init_vector)
     padded_data = pad(value.encode('utf-8'), AES.block_size)
     encrypted = cipher.encrypt(padded_data)
@@ -27,23 +41,19 @@ def encrypt_asymmetric(public_key, plaintext):
     encrypted_data = cipher.encrypt(plaintext.encode('utf-8'))
     return base64.b64encode(encrypted_data).decode('utf-8')
 
-def fetch_public_key_from_vault(cert_ocid):
-    signer = oci.auth.signers.get_resource_principals_signer()
-    try:
-        client = oci.secrets.SecretsClient({}, signer=signer)
-        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content
-        cert = load_pem_x509_certificate(cert_content, default_backend())
-        return cert.public_key()
-    except Exception as ex:
-        print("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
-        raise
-
-def encryption_logic(payload, cert_ocid):
+def encryption_logic(payload, key_ocid):
     randomno = generate_random(16)
     init_vector = generate_random(16)
-    public_key = fetch_public_key_from_vault(cert_ocid)
+    public_key_bytes = read_key_from_vault(key_ocid)
+    public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
+    public_key_str = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ).decode('utf-8')
 
-    encrypted_data = encrypt_symmetric(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
+    logging.info("Public key: {}".format(public_key_str))
+    public_key = load_private_key_from_string(public_key_str) 
+    encrypted_data = encrypt_symm(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
     encrypted_key = encrypt_asymmetric(public_key, randomno)
     
     return encrypted_data, encrypted_key
