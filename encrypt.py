e2a57926cc5411f4981a695830c6afbc0ffa7f9f
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-02 18:22:36 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,15 +1,9 @@
-import secrets
-import logging
 import string
 import base64
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Util.Padding import pad
 from Crypto.PublicKey import RSA
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.x509 import load_pem_x509_certificate
+from Crypto.Util.Padding import pad
+import secrets
 import oci
 
 def generate_random(length):
@@ -17,21 +11,7 @@ def generate_random(length):
     random_key = ''.join(secrets.choice(characters) for _ in range(length))
     return random_key
 
-def read_certificate_from_vault(cert_ocid):
-    signer = oci.auth.signers.get_resource_principals_signer()
-    try:
-        client = oci.secrets.SecretsClient({}, signer=signer)
-        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content
-        return cert_content
-    except Exception as ex:
-        logging.error("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
-        raise
-
-def load_public_key_from_certificate(cert_content):
-    cert = load_pem_x509_certificate(cert_content.encode('utf-8'), default_backend())
-    return cert.public_key()
-
-def encrypt_symm(key, init_vector, value):
+def encrypt_symmetric(key, init_vector, value):
     cipher = AES.new(key, AES.MODE_CBC, init_vector)
     padded_data = pad(value.encode('utf-8'), AES.block_size)
     encrypted = cipher.encrypt(padded_data)
@@ -39,24 +19,26 @@ def encrypt_symm(key, init_vector, value):
     return base64.urlsafe_b64encode(iv_and_ciphertext).decode('utf-8')
 
 def encrypt_asymmetric(public_key, plaintext):
-    ciphertext = public_key.encrypt(
-        plaintext.encode('utf-8'),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return base64.b64encode(ciphertext).decode('utf-8')
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_data = cipher.encrypt(plaintext.encode('utf-8'))
+    return base64.b64encode(encrypted_data).decode('utf-8')
+
+def fetch_public_key_from_vault(cert_ocid):
+    signer = oci.auth.signers.get_resource_principals_signer()
+    try:
+        client = oci.secrets.SecretsClient({}, signer=signer)
+        cert_content = client.get_secret_bundle(cert_ocid).data.secret_bundle_content.content
+        return RSA.import_key(cert_content)
+    except Exception as ex:
+        print("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
+        raise
 
 def encryption_logic(payload, cert_ocid):
     randomno = generate_random(16)
     init_vector = generate_random(16)
+    public_key = fetch_public_key_from_vault(cert_ocid)
 
-    cert_content = read_certificate_from_vault(cert_ocid)
-    public_key = load_public_key_from_certificate(cert_content)
-
-    encrypted_data = encrypt_symm(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
+    encrypted_data = encrypt_symmetric(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
     encrypted_key = encrypt_asymmetric(public_key, randomno)
     
     return encrypted_data, encrypted_key
