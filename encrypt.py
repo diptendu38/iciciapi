8f3241ea00075aab83cae123a02a44502fede657
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-02 17:27:46 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,18 @@
+import secrets
+import logging
+import string
 import base64
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from Crypto.Util.Padding import unpad
-import oci
-import logging
+from Crypto.Util.Padding import pad
 from cryptography.hazmat.backends import default_backend
-from cryptography import x509
+from Crypto.PublicKey import RSA
+from cryptography.hazmat.primitives import serialization
+import oci
 
-def load_certificate_from_string(cert_str):
-    cert_bytes = cert_str.encode('utf-8')
-    cert = x509.load_pem_x509_certificate(cert_bytes, default_backend())
-    return cert
-
-def extract_public_key_from_certificate(cert):
-    return cert.public_key()
+def generate_random(length):
+    characters = string.ascii_letters + string.digits
+    random_key = ''.join(secrets.choice(characters) for _ in range(length))
+    return random_key
 
 def read_certificate_from_vault(cert_ocid):
     signer = oci.auth.signers.get_resource_principals_signer()
@@ -25,34 +24,30 @@ def read_certificate_from_vault(cert_ocid):
         logging.error("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
         raise
 
-def decrypt_symmetric(key, iv, ciphertext):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    return decrypted.decode('utf-8')
+def load_public_key_from_certificate(cert_content):
+    public_key = serialization.load_pem_x509_certificate(cert_content, default_backend()).public_key()
+    return public_key
 
-def decrypt_asymmetric(b64_encrypted_msg, private_key):
-    cipher = PKCS1_OAEP.new(private_key)
-    decrypted_msg = cipher.decrypt(base64.b64decode(b64_encrypted_msg))
-    return decrypted_msg
+def encrypt_symm(key, init_vector, value):
+    cipher = AES.new(key, AES.MODE_CBC, init_vector)
+    padded_data = pad(value.encode('utf-8'), AES.block_size)
+    encrypted = cipher.encrypt(padded_data)
+    iv_and_ciphertext = init_vector + encrypted
+    return base64.urlsafe_b64encode(iv_and_ciphertext).decode('utf-8')
 
-def decryption_logic(encrypted_data, encrypted_key, cert_ocid):
+def encrypt_asymmetric(public_key, plaintext):
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_data = cipher.encrypt(plaintext.encode('utf-8'))
+    return base64.b64encode(encrypted_data).decode('utf-8')
+
+def encryption_logic(payload, cert_ocid):
+    randomno = generate_random(16)
+    init_vector = generate_random(16)
+
     cert_content = read_certificate_from_vault(cert_ocid)
-    cert = load_certificate_from_string(cert_content)
-    public_key = extract_public_key_from_certificate(cert)
+    public_key = load_public_key_from_certificate(cert_content)
 
-    plain_key = decrypt_asymmetric(encrypted_key, public_key)
-    print(f"Decrypted Session Key: {plain_key.decode('utf-8')}")
-
-    iv_and_ciphertext = base64.urlsafe_b64decode(encrypted_data.encode('utf-8'))
-    iv = iv_and_ciphertext[:16]
-    ciphertext = iv_and_ciphertext[16:]
-
-    decrypted_payload = decrypt_symmetric(plain_key, iv, ciphertext)
-    print(f"Decrypted Payload: {decrypted_payload}")
-'''
-# Example usage
-encrypted_data = "b0NUU01yZ0I1c1dhUTlhenAPu-HcssSM8m6Ph1_Ok0vEnisudpHUTT4m49WvZQYxrvw1xjlUOrThsdTHyFcIxg=="
-encrypted_key = "n07Ypu0+UFIFzwnL/PxA9Eu+oJ5j20AAuzQ5xLWMEGqDZwHEAFUejC7pQczLAYnp5M9DLAQ3s/fM34Kt/jwX0rFHLnEStNqtMQl7/NTv7mOzafKVb0/KYjGuJu6096OXhLHvjk/BDB1DzqqfCgaPndGCPh202aO2BKKon5WbZiQVrsaOIhJ9/SiRqETA48On/lk/NLpyvdZ/dqUsfwouB9Ni2JY9ouqPjIrynCip813rpIpRZnfnFiBP4/n7AICyIutBZKcds6dsXbxBZW4mqBWKldq2/omqxCbW6P8IqHwZ3atKLy9W8gfKhe1swFPClLJH8T9wjh/ThxLPNqJwaQ=="
-cert_ocid = "your_certificate_ocid"
-
-decryption_logic(encrypted_data, encrypted_key, cert_ocid)'''
+    encrypted_data = encrypt_symm(randomno.encode('utf-8'), init_vector.encode('utf-8'), payload)
+    encrypted_key = encrypt_asymmetric(public_key, randomno)
+    
+    return encrypted_data, encrypted_key
